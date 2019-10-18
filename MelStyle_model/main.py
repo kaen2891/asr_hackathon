0ac5bb87185f75eeb3192ffa5022224b45f9e547
kaen2891 <kaen2891@gmail.com>
2019-10-18 22:20:36 +0900

    add Mel40 VGG Style model

---
+++ MelStyle_model/main.py
@@ -0,0 +1,449 @@
+"""
+Copyright 2019-present NAVER Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import math
+import wavio
+import argparse
+import queue
+import shutil
+import random
+import math
+import time
+import torch
+import logging
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.optim as optim
+import Levenshtein as Lev 
+import random
+import spec_augment_pytorch
+import sparse_image_warp_pytorch
+
+import label_loader
+from loader import *
+from models.transformer import Model #3d use
+from models.utils import ScheduledOptim, LabelSmoothingLoss
+
+import nsml
+from nsml import GPU_NUM, DATASET_PATH, DATASET_NAME, HAS_DATASET
+
+char2index = dict()
+index2char = dict()
+SOS_token = 0
+EOS_token = 0
+PAD_token = 0
+
+if HAS_DATASET == False:
+    DATASET_PATH = './sample_dataset'
+
+DATASET_PATH = os.path.join(DATASET_PATH, 'train')
+
+def label_to_string(labels):
+    if len(labels.shape) == 1:
+        sent = str()
+        for i in labels:
+            if i.item() == EOS_token:
+                break
+            sent += index2char[i.item()]
+        return sent
+
+    elif len(labels.shape) == 2:
+        sents = list()
+        for i in labels:
+            sent = str()
+            for j in i:
+                if j.item() == EOS_token:
+                    break
+                sent += index2char[j.item()]
+            sents.append(sent)
+
+        return sents
+
+def char_distance(ref, hyp):
+    ref = ref.replace(' ', '') 
+    hyp = hyp.replace(' ', '') 
+
+    dist = Lev.distance(hyp, ref)
+    length = len(ref.replace(' ', ''))
+
+    return dist, length 
+
+def get_distance(ref_labels, hyp_labels, display=False):
+    total_dist = 0
+    total_length = 0
+    for i in range(len(ref_labels)):
+        ref = label_to_string(ref_labels[i])
+        hyp = label_to_string(hyp_labels[i])
+        dist, length = char_distance(ref, hyp)
+        total_dist += dist
+        total_length += length 
+        if display:
+            cer = total_dist / total_length
+            logger.debug('%d (%0.4f)\n(%s)\n(%s)' % (i, cer, ref, hyp))
+    return total_dist, total_length
+
+
+def train(model, total_batch_size, queue, criterion, optimizer, device, train_begin, train_loader_count, print_batch=5, teacher_forcing_ratio=1):
+    total_loss = 0.
+    total_num = 0
+    total_dist = 0
+    total_length = 0
+    total_sent_num = 0
+    batch = 0
+
+    model.train()
+
+    logger.info('train() start')
+
+    begin = epoch_begin = time.time()
+
+    while True:
+        if queue.empty():
+            logger.debug('queue is empty')
+
+        feats, scripts, feat_lengths, script_lengths = queue.get()
+        
+        rand_for_aug = random.random()
+        if rand_for_aug <= 0.5:
+            new_feats = feats.numpy()
+            new_feats = np.transpose(new_feats, (0, 2, 1))
+            
+            warped_masked_spectrogram = spec_augment_pytorch.spec_augment(mel_spectrogram=new_feats, frequency_mask_num=1) # LB
+            #warped_masked_spectrogram = spec_augment_pytorch.spec_augment(mel_spectrogram=new_feats, frequency_mask_num=2) # LD
+            feats = np.transpose(warped_masked_spectrogram, (0, 2, 1))
+            feats = torch.from_numpy(feats)
+            
+
+        if feats.shape[0] == 0:
+            # empty feats means closing one loader
+            train_loader_count -= 1
+
+            logger.debug('left train_loader: %d' % (train_loader_count))
+
+            if train_loader_count == 0:
+                break
+            else:
+                continue
+
+        optimizer.zero_grad()
+
+        feats = feats.to(device)
+        scripts = scripts.to(device)
+
+        src_len = scripts.size(1)
+        target = scripts[:, 1:].clone()
+
+        logit = model(feats, scripts[:,:-1], mode='train')
+
+        y_hat = logit.max(-1)[1]
+
+        loss = criterion(logit.contiguous().view(-1, logit.size(-1)), target.contiguous().view(-1))
+        total_loss += loss.item()
+        total_num += sum(feat_lengths)
+
+        display = random.randrange(0, 100) == 0
+        dist, length = get_distance(target, y_hat, display=display)
+        total_dist += dist
+        total_length += length
+
+        total_sent_num += target.size(0)
+
+        loss.backward()
+        optimizer.step_and_update_lr()
+
+        if batch % print_batch == 0:
+            current = time.time()
+            elapsed = current - begin
+            epoch_elapsed = (current - epoch_begin) / 60.0
+            train_elapsed = (current - train_begin) / 3600.0
+
+            logger.info('batch: {:4d}/{:4d}, loss: {:.4f}, cer: {:.2f}, elapsed: {:.2f}s {:.2f}m {:.2f}h'
+                .format(batch,
+                        #len(dataloader),
+                        total_batch_size,
+                        total_loss / total_num,
+                        total_dist / total_length,
+                        elapsed, epoch_elapsed, train_elapsed))
+            begin = time.time()
+
+            nsml.report(False,
+                        step=train.cumulative_batch_count, train_step__loss=total_loss/total_num,
+                        train_step__cer=total_dist/total_length)
+        batch += 1
+        train.cumulative_batch_count += 1
+
+    logger.info('train() completed')
+    return total_loss / total_num, total_dist / total_length
+
+
+train.cumulative_batch_count = 0
+
+
+def evaluate(model, dataloader, queue, criterion, device):
+    logger.info('evaluate() start')
+    total_loss = 0.
+    total_num = 0
+    total_dist = 0
+    total_length = 0
+    total_sent_num = 0
+
+    model.eval()
+
+    with torch.no_grad():
+        while True:
+            feats, scripts, feat_lengths, script_lengths = queue.get()
+            if feats.shape[0] == 0:
+                break
+
+            feats = feats.to(device)
+            scripts = scripts.to(device)
+
+            src_len = scripts.size(1)
+            target = scripts[:, 1:]
+
+
+            logit = model(feats, scripts[:,:-1], mode='eval')
+            logit = logit[:,:target.size(1), :]
+            
+            y_hat = logit.max(-1)[1]
+
+            loss = criterion(logit.contiguous().view(-1, logit.size(-1)), target.contiguous().view(-1))
+            total_loss += loss.item()
+            total_num += sum(feat_lengths)
+
+            display = random.randrange(0, 100) == 0
+            dist, length = get_distance(target, y_hat, display=display)
+            total_dist += dist
+            total_length += length
+            total_sent_num += target.size(0)
+
+    logger.info('evaluate() completed')
+    return total_loss / total_num, total_dist / total_length
+
+def bind_model(model, optimizer=None):
+    def load(filename, **kwargs):
+        state = torch.load(os.path.join(filename, 'model.pt'))
+        model.load_state_dict(state['model'])
+        if 'optimizer' in state and optimizer:
+            optimizer.load_state_dict(state['optimizer'])
+        print('Model loaded')
+
+    def save(filename, **kwargs):
+        state = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict()
+        }
+        torch.save(state, os.path.join(filename, 'model.pt'))
+
+    def infer(wav_path):
+        model.eval()
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        #input = get_spectrogram_feature(wav_path).unsqueeze(0)
+        input = get_mel_feature(wav_path).unsqueeze(0)
+        input = input.to(device)
+
+        logit = model(input, None, mode='eval')
+
+        y_hat = logit.max(-1)[1]
+        hyp = label_to_string(y_hat)
+
+        return hyp[0]
+
+    nsml.bind(save=save, load=load, infer=infer) # 'nsml.bind' function must be called at the end.
+
+#def split_dataset(config, wav_paths, script_paths, valid_ratio=0.05):
+def split_dataset(config, wav_paths, script_paths, valid_ratio=0.005): # 0.05 -> 0.005: 150samples
+    train_loader_count = config.workers
+    records_num = len(wav_paths) # 30000
+    batch_num = math.ceil(records_num / config.batch_size) # 469
+
+    valid_batch_num = math.ceil(batch_num * valid_ratio) # 24
+    train_batch_num = batch_num - valid_batch_num # 445
+
+    batch_num_per_train_loader = math.ceil(train_batch_num / config.workers)
+
+    train_begin = 0
+    train_end_raw_id = 0
+    train_dataset_list = list()
+
+    for i in range(config.workers):
+
+        train_end = min(train_begin + batch_num_per_train_loader, train_batch_num)
+
+        train_begin_raw_id = train_begin * config.batch_size
+        train_end_raw_id = train_end * config.batch_size
+
+        train_dataset_list.append(BaseDataset(
+                                        wav_paths[train_begin_raw_id:train_end_raw_id],
+                                        script_paths[train_begin_raw_id:train_end_raw_id],
+                                        SOS_token, EOS_token))
+        train_begin = train_end 
+
+    valid_dataset = BaseDataset(wav_paths[train_end_raw_id:], script_paths[train_end_raw_id:], SOS_token, EOS_token)
+
+    return train_batch_num, train_dataset_list, valid_dataset
+
+def main():
+
+    global char2index
+    global index2char
+    global SOS_token
+    global EOS_token
+    global PAD_token
+
+    parser = argparse.ArgumentParser(description='Speech hackathon Baseline')
+    parser.add_argument('--hidden_size', type=int, default=512, help='hidden size of model (default: 256)')
+    parser.add_argument('--layer_size', type=int, default=3, help='number of layers of model (default: 3)')
+    parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate in training (default: 0.2)')
+    parser.add_argument('--bidirectional', action='store_true', help='use bidirectional RNN for encoder (default: False)')
+    parser.add_argument('--use_attention', action='store_true', help='use attention between encoder-decoder (default: False)')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size in training (default: 32)')
+    parser.add_argument('--workers', type=int, default=4, help='number of workers in dataset loader (default: 4)')
+    parser.add_argument('--max_epochs', type=int, default=10, help='number of max epochs in training (default: 10)')
+    parser.add_argument('--lr', type=float, default=1e-7, help='learning rate (default: 0.0001)')
+    parser.add_argument('--teacher_forcing', type=float, default=0.5, help='teacher forcing ratio in decoder (default: 0.5)')
+    parser.add_argument('--max_len', type=int, default=80, help='maximum characters of sentence (default: 80)')
+    parser.add_argument('--no_cuda', action='store_true', default=False, help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
+    parser.add_argument('--save_name', type=str, default='model', help='the name of model in nsml or local')
+    parser.add_argument('--mode', type=str, default='train')
+    parser.add_argument("--pause", type=int, default=0)
+
+    args = parser.parse_args()
+
+    char2index, index2char = label_loader.load_label('./hackathon.labels')
+    SOS_token = char2index['<s>']
+    EOS_token = char2index['</s>']
+    PAD_token = char2index['_']
+
+    char2index['[MASK]'] = len(char2index)
+    index2char[len(index2char)] = '[MASK]'
+
+    MASK_token = char2index['[MASK]']
+
+
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device('cuda' if args.cuda else 'cpu')
+
+    # N_FFT: defined in loader.py
+    feature_size = N_FFT / 2
+
+    d_model_size = 512
+
+    model = Model(len(char2index), SOS_token, EOS_token, d_model=d_model_size, nhead=8, max_seq_len=1024, 
+                                                         num_encoder_layers=0, num_decoder_layers=4,
+                                                         enc_feedforward=2048, dec_feedforward=2048,
+                                                         dropout=0.1, padding_idx=PAD_token, mask_idx=MASK_token, device=device)
+    def count_parameters(model):
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f'Model parameters : {count_parameters(model):,}')
+
+    model = nn.DataParallel(model).to(device)
+
+    #optimizer = optim.Adam(model.module.parameters(), lr=args.lr)
+    optimizer = ScheduledOptim(optim.Adam(
+            filter(lambda x: x.requires_grad, model.parameters()),
+            betas=(0.9, 0.98), eps=1e-09, weight_decay=1e-3),
+            d_model_size, 4000)
+
+    #criterion = nn.NLLLoss(reduction='sum', ignore_index=PAD_token).to(device)
+    criterion = LabelSmoothingLoss(0.1, len(char2index), ignore_index=PAD_token).to(device)
+
+    bind_model(model, optimizer)
+
+    if args.pause == 1:
+        nsml.paused(scope=locals())
+
+    if args.mode != "train":
+        return
+
+    # For Loading...
+    #nsml.load(checkpoint='best', session="team185/sr-hack-2019-dataset/501")
+    #nsml.save(args.save_name)
+    #exit()
+
+
+    data_list = os.path.join(DATASET_PATH, 'train_data', 'data_list.csv')
+    wav_paths = list()
+    script_paths = list()
+
+    with open(data_list, 'r') as f:
+        for line in f:
+
+            wav_path, script_path = line.strip().split(',')
+            wav_paths.append(os.path.join(DATASET_PATH, 'train_data', wav_path))
+            script_paths.append(os.path.join(DATASET_PATH, 'train_data', script_path))
+
+    best_loss = 1e10
+    begin_epoch = 0
+
+    # load all target scripts for reducing disk i/o
+    target_path = os.path.join(DATASET_PATH, 'train_label')
+    load_targets(target_path)
+
+    #train_batch_num, train_dataset_list, valid_dataset = split_dataset(args, wav_paths, script_paths, valid_ratio=0.05)
+    train_batch_num, train_dataset_list, valid_dataset = split_dataset(args, wav_paths, script_paths, valid_ratio=0.005) # 0.05 -> 0.005
+
+    logger.info('start')
+
+    train_begin = time.time()
+
+    for epoch in range(begin_epoch, args.max_epochs):
+
+        train_queue = queue.Queue(args.workers * 2)
+
+        train_loader = MultiLoader(train_dataset_list, train_queue, args.batch_size, args.workers)
+        train_loader.start()
+
+        train_loss, train_cer = train(model, train_batch_num, train_queue, criterion, optimizer, device, train_begin, args.workers, 10, args.teacher_forcing)
+        logger.info('Epoch %d (Training) Loss %0.4f CER %0.4f' % (epoch, train_loss, train_cer))
+
+        train_loader.join()
+
+        valid_queue = queue.Queue(args.workers * 2)
+        valid_loader = BaseDataLoader(valid_dataset, valid_queue, args.batch_size, 0)
+        valid_loader.start()
+
+        eval_loss, eval_cer = evaluate(model, valid_loader, valid_queue, criterion, device)
+        logger.info('Epoch %d (Evaluate) Loss %0.4f CER %0.4f' % (epoch, eval_loss, eval_cer))
+
+        valid_loader.join()
+
+        nsml.report(False,
+            step=epoch, train_epoch__loss=train_loss, train_epoch__cer=train_cer,
+            eval__loss=eval_loss, eval__cer=eval_cer)
+
+        best_model = (eval_loss < best_loss)
+        #nsml.save(args.save_name)
+        nsml.save(args.save_name+str(epoch)) # save each epochs
+
+        if best_model:
+            nsml.save('best')
+            best_loss = eval_loss
+
+if __name__ == "__main__":
+    main()
